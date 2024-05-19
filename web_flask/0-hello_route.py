d74076e027953e6d4ac7d20669695a9aeb5a6717
KatliKoma <katlegokoma1@gmail.com>
2024-05-19 22:39:20 +0200

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from flask import Flask
 
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Route that displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
