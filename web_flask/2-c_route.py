942f3467878ce0f9e7fbdbcf0d4eb4d484a87122
KatliKoma <katlegokoma1@gmail.com>
2024-05-19 22:59:57 +0200

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
 """Script to start a Flask web application with three view functions."""
 
+#!/usr/bin/python3
+""" 2. Script to start a Flask web application with 3 view functions """
+
 from flask import Flask
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello_world():
-    """Returns a greeting message."""
+    """ Returns some text. """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hello():
-    """Returns another greeting message."""
+    """ Return other text. """
     return 'HBNB'
+
 
 @app.route('/c/<text>')
 def c_text(text):
-    """Displays 'C' followed by the value of the text variable."""
+    """ replace text with variable. """
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
